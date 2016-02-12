85692e2c4dcd77a640ae6658b723463ba6c13886
Dmitri Pisarev <dimaip@gmail.com>
2016-02-12 13:02:06 +0300

    Fix gulp

--- Packages/Sites/Sfi.Site/Frontend/Gulpfile.js
+++ Packages/Sites/Sfi.Site/Frontend/Gulpfile.js
@@ -15,14 +15,12 @@ var inputVendorJs = [
 	'./node_modules/scrollreveal/dist/scrollReveal.min.js'
 ];
 var inputVendorCss = [
-	'./node_modules/owl-carousel-2/assets/owl.carousel.min.css',
-	'./node_modules/owl-carousel-2/assets/owl.theme.default.min.css'
+	'./node_modules/owl-carousel-2/assets/owl.carousel.min.css'
 ];
 var inputMyJs = ['./js/**/*.js'];
 var inputMySass = ['./scss/**/*.scss'];
 
-var allInputs = [].concat(inputVendorJs, inputMyJs, inputVendorCss, inputMySass);
-var allTasks = ['vendorJs', 'myJs', 'vendorCss', 'mySass'];
+var allTasks = ['mySass', 'vendorJs', 'myJs', 'vendorCss'];
 var output = './built';
 
 gulp.task('serve', allTasks, function () {
@@ -30,7 +28,11 @@ gulp.task('serve', allTasks, function () {
 		proxy: 'dev.bratstvo-movie.loc'
 	});
 
-	gulp.watch(allInputs, allTasks)
+	gulp.watch(inputMySass, ['mySass'])
+		.on('change', function (event) {
+			console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+		});
+	gulp.watch(inputMyJs, ['myJs'])
 		.on('change', function (event) {
 			console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 		});
@@ -41,24 +43,21 @@ gulp.task('vendorJs', function () {
 		.src(inputVendorJs)
 		.pipe(concatJs('vendor.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest(output))
-		.pipe(browserSync.stream());
+		.pipe(gulp.dest(output));
 });
 gulp.task('myJs', function () {
 	return gulp
 		.src(inputMyJs)
 		.pipe(concatJs('index.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest(output))
-		.pipe(browserSync.stream());
+		.pipe(gulp.dest(output));
 });
 gulp.task('vendorCss', function () {
 	return gulp
 		.src(inputVendorCss)
 		.pipe(concatCss('vendor.css'))
 		.pipe(cssnano())
-		.pipe(gulp.dest(output))
-		.pipe(browserSync.stream());
+		.pipe(gulp.dest(output));
 });
 var sassOptions = {
 	errLogToConsole: true,
