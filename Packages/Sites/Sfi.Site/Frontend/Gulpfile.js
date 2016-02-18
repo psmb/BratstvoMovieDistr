cb763675459b557385ae636d630a3c6e3a6ccaec
Dmitri Pisarev <dimaip@gmail.com>
2016-02-18 09:03:11 +0300

    Setup sourcemaps and vendor stuff

--- Packages/Sites/Sfi.Site/Frontend/Gulpfile.js
+++ Packages/Sites/Sfi.Site/Frontend/Gulpfile.js
@@ -7,21 +7,26 @@ var uglify = require('gulp-uglify');
 var concatJs = require('gulp-concat');
 var concatCss = require('gulp-concat-css');
 var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
 
 // Input configuration
 var inputJs = [
 	'./node_modules/jquery/dist/jquery.min.js',
-	'./node_modules/owl-carousel-2/owl.carousel.min.js',
 	'./node_modules/scrollreveal/dist/scrollReveal.min.js',
+	'./node_modules/owl-carousel-2/owl.carousel.min.js',
 	'./node_modules/mobile-detect/mobile-detect.min.js',
+	'./node_modules/lazyloadxt/dist/jquery.lazyloadxt.extra.min.js',
+	'./vendor_js/**/*.js',
 	'./js/**/*.js'
 ];
 var inputVendorCss = [
-	'./node_modules/owl-carousel-2/assets/owl.carousel.min.css'
+	'./node_modules/lazyloadxt/dist/jquery.lazyloadxt.spinner.css',
+	'./node_modules/owl-carousel-2/assets/owl.carousel.min.css',
+	'./vendor_css/**/*.css'
 ];
-var inputMySass = ['./scss/**/*.scss'];
+var inputSass = ['./scss/**/*.scss'];
 
-var allTasks = ['mySass', 'js', 'vendorCss'];
+var allTasks = ['sass', 'js', 'vendorCss'];
 var output = './built';
 
 gulp.task('serve', allTasks, function () {
@@ -29,7 +34,7 @@ gulp.task('serve', allTasks, function () {
 		proxy: 'dev.bratstvo-movie.loc'
 	});
 
-	gulp.watch(inputMySass, ['mySass'])
+	gulp.watch(inputSass, ['sass'])
 		.on('change', function (event) {
 			console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
 		});
@@ -42,28 +47,34 @@ gulp.task('serve', allTasks, function () {
 gulp.task('js', function () {
 	return gulp
 		.src(inputJs)
+		.pipe(sourcemaps.init())
 		.pipe(concatJs('index.js'))
 		.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(output));
 });
 gulp.task('vendorCss', function () {
 	return gulp
 		.src(inputVendorCss)
+		.pipe(sourcemaps.init())
 		.pipe(concatCss('vendor.css'))
 		.pipe(cssnano())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(output));
 });
 var sassOptions = {
 	errLogToConsole: true,
 	outputStyle: 'expanded'
 };
-gulp.task('mySass', function () {
+gulp.task('sass', function () {
 	return gulp
-		.src(inputMySass)
+		.src(inputSass)
 		.pipe(sassGlob())
+		.pipe(sourcemaps.init())
 		.pipe(sass(sassOptions).on('error', sass.logError))
 		.pipe(autoprefixer())
 		.pipe(cssnano())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(output))
 		.pipe(browserSync.stream());
 });
