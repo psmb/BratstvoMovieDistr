046274d4106080467188099cdae0e1c4637f2fdb
Dmitri Pisarev <dimaip@gmail.com>
2016-03-03 22:24:10 +0300

    FIX youtube api

--- Packages/Sites/Sfi.Site/Frontend/js_sync/trailer.js
+++ Packages/Sites/Sfi.Site/Frontend/js_sync/trailer.js
@@ -1,3 +1,9 @@
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 var trailer;
 var md = new MobileDetect(window.navigator.userAgent);
 
